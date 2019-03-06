1d8ccdd02b0ec10e24407d90cd07117cae723420
alex <skainrusak@gmail.com>
2019-03-06 19:33:34 +0600

    ColdCalling new number

--- app/src/main/java/com/example/waves/zamza/MainMenu.java
+++ app/src/main/java/com/example/waves/zamza/MainMenu.java
@@ -1,17 +1,22 @@
 package com.example.waves.zamza;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
     protected void  onCreate (Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.main_menu);
+    setContentView(R.layout.fragment_added_number);
     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
  }
  public void cold_calling (View view){
@@ -29,6 +34,18 @@ public void debt_repayment_term(View view){
     public void sales_training(View view){
         Intent sales = new Intent(this ,SalesTraining.class);
         startActivity(sales);
+    }
+    public void createContact (){
+        Uri rawContactUri = getContentResolver().insert(ContactsContract.RawContacts.CONTENT_URI, new ContentValues());
+        long rowContacrId = ContentUris.parseId(rawContactUri);
+        ContentValues values = new ContentValues();
+        values.put(ContactsContract.Data.RAW_CONTACT_ID,rowContacrId);
+        values.put(ContactsContract.Data.MIMETYPE , ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
+        values.put(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, "Robert Smith");
+        getContentResolver().insert(ContactsContract.Data.CONTENT_URI, values);
+    }
+    public void test (View view){
+createContact();
     }
     public void loadFragment (Fragment fragment){
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
