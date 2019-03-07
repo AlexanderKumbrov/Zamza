bc0e0a5f8330dc55471a34a8ed93e81f7013ce21
alex <skainrusak@gmail.com>
2019-03-07 20:25:49 +0600

    ContactName conclusion

--- app/src/main/java/com/example/waves/zamza/MainMenu.java
+++ app/src/main/java/com/example/waves/zamza/MainMenu.java
@@ -1,23 +1,31 @@
 package com.example.waves.zamza;
 
+import android.app.Activity;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
+    private TextView textView;
+    String name ;
+    String number ;
     protected void  onCreate (Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
     setContentView(R.layout.fragment_added_number);
     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    textView = (TextView)findViewById(R.id.name_contact);
  }
  public void cold_calling (View view){
      Intent cold = new Intent(this , ColdCallingActivity.class);
@@ -43,9 +51,27 @@ public void debt_repayment_term(View view){
         values.put(ContactsContract.Data.MIMETYPE , ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
         values.put(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, "Robert Smith");
         getContentResolver().insert(ContactsContract.Data.CONTENT_URI, values);
+
     }
     public void test (View view){
-createContact();
+
+Intent callIntent = new Intent(Intent.ACTION_PICK , ContactsContract.Contacts.CONTENT_URI);
+startActivityForResult(callIntent , 1 );
+
+    }
+    @Override
+    public void onActivityResult (int reqCode , int resultCode , Intent data){
+        super.onActivityResult(reqCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            Uri contactData = data.getData();
+            Cursor c =  managedQuery(contactData, null, null, null, null);
+            if (c.moveToFirst()) {
+                 name = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
+
+            }
+        }
+
+textView.setText(name);
     }
     public void loadFragment (Fragment fragment){
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
