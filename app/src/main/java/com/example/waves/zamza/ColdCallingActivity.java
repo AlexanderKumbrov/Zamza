0fe22c6274da1c09a3bb736730b571f1b62fbfa9
Alex <skainrusak@gmail.com>
2019-04-18 17:08:05 +0600

    fix  bug

--- app/src/main/java/com/example/waves/zamza/ColdCallingActivity.java
+++ app/src/main/java/com/example/waves/zamza/ColdCallingActivity.java
@@ -3,16 +3,21 @@ package com.example.waves.zamza;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
 import java.util.UUID;
 
 public class ColdCallingActivity extends CreateFragment {
 
     private static final String EXTRA_CALLING_ID = "com.example.waves.zamza.callingId";
+    private ViewPager mViewPager;
+    private List <ColdCalling>mColdCalling;
+
     public static Intent newIntent (Context packageContext , UUID callingId){
-        Intent intent= new Intent(packageContext , ColdCallingFragment.class);
+        Intent intent= new Intent(packageContext , ColdCallingActivity.class);
         intent.putExtra(EXTRA_CALLING_ID , callingId);
         return intent;
     }
