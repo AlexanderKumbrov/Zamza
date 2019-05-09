34fbf6c23f32b896f8132e37fb49e804e5a6092e
Alex <skainrusak@gmail.com>
2019-05-09 18:41:07 +0600

    fix bug for android 8-9

--- app/src/main/java/com/example/waves/zamza/ColdCallingFragment.java
+++ app/src/main/java/com/example/waves/zamza/ColdCallingFragment.java
@@ -71,7 +71,7 @@ getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         positionContact = (EditText)view.findViewById(R.id.position_contact_edit);
         companyContact = (EditText)view.findViewById(R.id.company_contact_edit);
         mailContact = (EditText)view.findViewById(R.id.mail_contact_edit);
-        relativeLayout = (RelativeLayout)view.findViewById(R.id.cold_calling_rv);
+
         mailContact.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -152,14 +152,14 @@ getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
             }
         });
+        relativeLayout = (RelativeLayout)view.findViewById(R.id.cold_calling_rv);
+
         callButton = (Button)view.findViewById(R.id.call_contact);
         callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_DIAL , Uri.parse("tel:" + mColdCalling.getNumberCalling()));
                 startActivity(intent);
-                    callButton.setText(R.string.the_call_is_made);
-                    callButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.call_is_made_call_button));
                 mColdCalling.setCallComplete(true);
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
                 View mView = getLayoutInflater().inflate(R.layout.dialog_result_call , null);
@@ -181,8 +181,9 @@ getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
                     @Override
                     public void onClick(View v) {
                         mColdCalling.setResultCall(true);
+                        callButton.setText(R.string.good_call);
+                        callButton.setBackgroundResource(R.drawable.good_call);
                         dialog.dismiss();
-                       relativeLayout.setBackgroundColor(Color.rgb(46, 204, 113));
                     }
                 });
                 notSuccessfulButton = (Button)mView.findViewById(R.id.not_successful_button);
@@ -190,7 +191,8 @@ getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
                     @Override
                     public void onClick(View v) {
                         mColdCalling.setResultCall(false);
-                        relativeLayout.setBackgroundColor(Color.rgb(231, 76, 60));
+                        callButton.setText(R.string.not_good_call);
+                        callButton.setBackgroundResource(R.drawable.not_a_good_call);
                         dialog.dismiss();
                     }
                 });
@@ -205,17 +207,15 @@ getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         });
 
 
-        if (mColdCalling.isCallComplete()){
-            callButton.setText(R.string.the_call_is_made);
-            callButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.call_is_made_call_button));
-            if (mColdCalling.isResultCall()){
-                relativeLayout.setBackgroundColor(Color.rgb(46, 204, 113));
-            }
-            else {
-                relativeLayout.setBackgroundColor(Color.rgb(231, 76, 60));
+            if (mColdCalling.isResultCall()) {
+                callButton.setText(R.string.good_call);
+                callButton.setBackgroundResource(R.drawable.good_call);
+            } else {
+                callButton.setText(R.string.not_good_call);
+                callButton.setBackgroundResource(R.drawable.not_a_good_call);
             }
 
-        }
+
         nameContact.setText(mColdCalling.getNameCalling());
         numberContact.setText(mColdCalling.getNumberCalling());
         positionContact.setText(mColdCalling.getPositionCalling());
