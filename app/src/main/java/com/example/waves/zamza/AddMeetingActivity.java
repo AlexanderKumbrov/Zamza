61f2ca8a2afaa6c006631a61cd3cb33ccede12c0
Alex <skainrusak@gmail.com>
2019-05-14 18:32:09 +0600

    Delete FragmentMeeting

--- app/src/main/java/com/example/waves/zamza/AddMeetingActivity.java
+++ app/src/main/java/com/example/waves/zamza/AddMeetingActivity.java
@@ -1,14 +1,30 @@
 package com.example.waves.zamza;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AddMeetingActivity extends CreateFragment {
+import java.util.UUID;
 
-    @Override
-    public Fragment createFragment() {
-        return new AddMeetingFragment();
+public class AddMeetingActivity extends AppCompatActivity {
+    private Meeting meeting;
+    private static final String EXTRA_CALL_ID ="com.example.waves.zamza.id";
+    public static Intent newIntent (Context context , UUID meetingId){
+        Intent intent = new Intent(context , AddMeetingActivity.class);
+        intent.putExtra(EXTRA_CALL_ID , meetingId);
+        return intent;
     }
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_fragment_meeting);
 
 }
+
+}
+
+
+
